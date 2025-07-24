0deb062f2ee6b98dfebbb8d2506ba793abc7afff
Matt Wunschel <matt.wunschel@caci.com>
2025-07-24 15:40:29 +0000

    Initial

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,27 +1,190 @@
+-- lua/plugins/java.lua
 return {
-	"nvim-java/nvim-java",
+	"mfussenegger/nvim-jdtls",
+	ft = { "java" },
 	dependencies = {
-		{
-			"neovim/nvim-lspconfig",
-			config = {},
-			opts = {
-				servers = {
-					jdtls = {
-						settings = {
-							java = {
-								configuration = {
-									runtimes = {
-										{
-											name = "JavaSE-21",
-											path = "/home/vagrant/.sdkman/candidates/java/current",
-										},
-									},
+		"neovim/nvim-lspconfig",
+		"mason.nvim",
+	},
+	config = function()
+		local jdtls = require("jdtls")
+
+		-- Function to find the root directory of the Java project
+		local function get_jdtls_root()
+			return require("jdtls.setup").find_root({
+				"gradlew",
+				"mvnw",
+				".git",
+				"pom.xml",
+				"build.gradle",
+				"build.gradle.kts",
+				"settings.gradle",
+				"settings.gradle.kts",
+			})
+		end
+
+		-- Function to get workspace directory
+		local function get_workspace_dir()
+			local project_name = vim.fn.fnamemodify(get_jdtls_root() or vim.fn.getcwd(), ":p:h:t")
+			return vim.fn.stdpath("data") .. "/workspace/" .. project_name
+		end
+
+		-- Configuration for jdtls
+		local function get_jdtls_config()
+			local home = os.getenv("HOME")
+			local java_home = "/home/vagrant/.sdkman/candidates/java/current" -- Your Java path
+			local mason_path = vim.fn.stdpath("data") .. "/mason"
+
+			-- Find the jdtls installation path
+			local jdtls_path = mason_path .. "/packages/jdtls"
+			local jdtls_bin = jdtls_path .. "/bin/jdtls"
+
+			-- Get blink.cmp capabilities
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			local config = {
+				cmd = {
+					java_home .. "/bin/java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=ALL",
+					"-Xmx2g",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens",
+					"java.base/java.util=ALL-UNNAMED",
+					"--add-opens",
+					"java.base/java.lang=ALL-UNNAMED",
+					"-jar",
+					vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+					"-configuration",
+					jdtls_path .. "/config_linux", -- Use config_mac on macOS, config_win on Windows
+					"-data",
+					get_workspace_dir(),
+				},
+
+				root_dir = get_jdtls_root(),
+
+				capabilities = capabilities,
+
+				settings = {
+					java = {
+						eclipse = {
+							downloadSources = true,
+						},
+						configuration = {
+							updateBuildConfiguration = "interactive",
+							runtimes = {
+								{
+									name = "JavaSE-21",
+									path = java_home,
 								},
 							},
 						},
+						maven = {
+							downloadSources = true,
+						},
+						implementationsCodeLens = {
+							enabled = true,
+						},
+						referencesCodeLens = {
+							enabled = true,
+						},
+						references = {
+							includeDecompiledSources = true,
+						},
+						format = {
+							enabled = true,
+							settings = {
+								url = vim.fn.stdpath("config") .. "/lang-servers/intellij-java-google-style.xml",
+								profile = "GoogleStyle",
+							},
+						},
+					},
+					signatureHelp = { enabled = true },
+					completion = {
+						favoriteStaticMembers = {
+							"org.hamcrest.MatcherAssert.assertThat",
+							"org.hamcrest.Matchers.*",
+							"org.hamcrest.CoreMatchers.*",
+							"org.junit.jupiter.api.Assertions.*",
+							"java.util.Objects.requireNonNull",
+							"java.util.Objects.requireNonNullElse",
+							"org.mockito.Mockito.*",
+						},
+						importOrder = {
+							"java",
+							"javax",
+							"com",
+							"org",
+						},
+					},
+					extendedClientCapabilities = jdtls.extendedClientCapabilities,
+					sources = {
+						organizeImports = {
+							starThreshold = 9999,
+							staticStarThreshold = 9999,
+						},
+					},
+					codeGeneration = {
+						toString = {
+							template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+						},
+						useBlocks = true,
 					},
 				},
-			},
-		},
-	},
+
+				flags = {
+					allow_incremental_sync = true,
+				},
+
+				init_options = {
+					bundles = {},
+				},
+
+				on_attach = function(client, bufnr)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+					-- Set up keymaps (same as your existing LSP keymaps)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+					end
+
+					-- Java-specific keymaps
+					map("<leader>co", jdtls.organize_imports, "Organize Imports")
+					map("<leader>crv", jdtls.extract_variable, "Extract Variable")
+					map("<leader>crc", jdtls.extract_constant, "Extract Constant")
+					map("<leader>crm", jdtls.extract_method, "Extract Method", { "n", "v" })
+
+					-- Standard LSP keymaps (if not already set globally)
+					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("grr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
+					map("gri", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
+					map("grd", require("fzf-lua").lsp_definitions, "[G]oto [D]efinition")
+					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("grt", require("fzf-lua").lsp_typedefs, "[G]oto [T]ype Definition")
+
+					-- Enable inlay hints if supported
+					if client.supports_method("textDocument/inlayHint") then
+						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+					end
+				end,
+			}
+
+			return config
+		end
+
+		-- Auto command to start jdtls for Java files
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = function()
+				local config = get_jdtls_config()
+				jdtls.start_or_attach(config)
+			end,
+		})
+	end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/lsp.lua
 M = {
 	-- Main LSP Configuration
 	"neovim/nvim-lspconfig",
@@ -8,7 +9,6 @@ M = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"mason-org/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"nvim-java/nvim-java",
 
 		-- Useful status updates for LSP.
 		{ "j-hui/fidget.nvim", opts = {} },
@@ -17,31 +17,6 @@ M = {
 		"saghen/blink.cmp",
 	},
 	config = function()
-		-- Brief aside: **What is LSP?**
-		--
-		-- LSP is an initialism you've probably heard, but might not understand what it is.
-		--
-		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
-		-- and language tooling communicate in a standardized fashion.
-		--
-		-- In general, you have a "server" which is some tool built to understand a particular
-		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-		-- processes that communicate with some "client" - in this case, Neovim!
-		--
-		-- LSP provides Neovim with features like:
-		--  - Go to definition
-		--  - Find references
-		--  - Autocompletion
-		--  - Symbol Search
-		--  - and more!
-		--
-		-- Thus, Language Servers are external tools that must be installed separately from
-		-- Neovim. This is where `mason` and related plugins come into play.
-		--
-		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-		-- and elegantly composed help section, `:help lsp-vs-treesitter`
-
 		--  This function gets run when an LSP attaches to a particular buffer.
 		--    That is to say, every time a new file is opened that is associated with
 		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -66,7 +41,6 @@ M = {
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
 				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-				-- map('gra', require('fzf-lua').lsp_code_actions, '[G]oto Code [A]ction', { 'n', 'x' })
 
 				-- Find references for the word under your cursor.
 				map("grr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
@@ -195,21 +169,6 @@ M = {
 		--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		require("java").setup({
-			jdk = {
-				auto_install = false,
-			},
-			root_markers = {
-				"settings.gradle",
-				"settings.gradle.kts",
-				"pom.xml",
-				"build.gradle",
-				"mvnw",
-				"gradlew",
-				"build.gradle",
-				"build.gradle.kts",
-			},
-		})
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -224,42 +183,9 @@ M = {
 			angularls = {},
 			bashls = {},
 			groovyls = {},
-			-- clangd = {},
-			-- gopls = {},
-			-- pyright = {},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			-- ts_ls = {},
-			--
-
-			jdtls = {
-				servers = {
-					jdtls = {
-						settings = {
-							java = {
-								configuration = {
-									runtimes = {
-										{
-											name = "JavaSE-21",
-											path = "/home/vagrant/.sdkman/candidates/java/current",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
+			-- NOTE: jdtls is handled separately in java.lua, so we don't include it here
 
 			lua_ls = {
-				-- cmd = { ... },
-				-- filetypes = { ... },
-				-- capabilities = {},
 				settings = {
 					Lua = {
 						completion = {
@@ -279,14 +205,9 @@ M = {
 		--    :Mason
 		--
 		-- You can press `g?` for help in this menu.
-		--
-		-- `mason` had to be setup earlier: to configure its options see the
-		-- `dependencies` table for `nvim-lspconfig` above.
-		--
-		-- You can add other tools here that you want Mason to install
-		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
+			"jdtls", -- Java LSP server
 			"stylua", -- Used to format Lua code
 			"prettierd",
 			"google-java-format",
@@ -299,6 +220,11 @@ M = {
 			automatic_installation = false,
 			handlers = {
 				function(server_name)
+					-- Skip jdtls as it's handled by nvim-jdtls plugin
+					if server_name == "jdtls" then
+						return
+					end
+
 					local server = servers[server_name] or {}
 					-- This handles overriding only values explicitly passed
 					-- by the server configuration above. Useful when disabling
