cc11c8f05f3a94a83dfa5f7f80948dfd92de0570
Matt Wunschel <matt.wunschel@caci.com>
2025-07-25 15:31:50 +0000

    Terminal mode edits

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,13 +8,19 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 -- vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, { desc = 'Open diagnostic float list' })
 
+-- Terminal keymaps
+vim.keymap.set("n", "<leader>t", ":terminal<CR>", { desc = "Open terminal" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Esc><Esc>", function()
+    vim.cmd("stopinsert")
+    vim.bo.modifiable = true
+end, { desc = "Exit terminal mode" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
