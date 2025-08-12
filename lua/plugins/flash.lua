0e99be60dd779f1eab81fcda5e6b047941a75fbc
Matt Wunschel <matt.wunschel@caci.com>
2025-08-12 19:35:43 +0000

    Keymap fix flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -17,8 +17,8 @@ require("flash").setup({
 })
 
 local map = require("utils").map
-map("s", require("flash").jump, "Flash", { mode = { "n", "x", "o" } })
-map("S", require("flash").treesitter, "Flash Treesitter", { mode = { "n", "x", "o" } })
-map("r", require("flash").remote, "Remote Flash", { mode = "o" })
-map("R", require("flash").treesitter_search, "Treesitter Search", { mode = { "o", "x" } })
-map("<c-s>", require("flash").toggle, "Toggle Flash Search", { mode = { "c" } })
+map("s", require("flash").jump, "Flash", { "n", "x", "o" })
+map("S", require("flash").treesitter, "Flash Treesitter", { "n", "x", "o" })
+map("r", require("flash").remote, "Remote Flash", "o")
+map("R", require("flash").treesitter_search, "Treesitter Search", { "o", "x" })
+map("<c-s>", require("flash").toggle, "Toggle Flash Search", "c")
